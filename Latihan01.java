a4a320cd5002aad744dbb26a9f565affa2dcaf71
RenWolfstein <mbimam03@gmail.com>
2023-05-13 15:24:51 +0800

    Initial commit

---
+++ Latihan01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pertemuan04.latihan01;
+
+/**
+ *
+ * @author USER
+ */
+public class Latihan01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
